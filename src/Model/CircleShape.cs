de9c8be6eb460869444402e1728ecece2371f584
Nikolay <nikolayvf@gmail.com>
2024-06-16 00:32:38 +0300

    new

---
+++ src/Model/CircleShape.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Draw.src.Model
+{
+    [Serializable]
+    internal class CircleShape : Shape
+    {
+        public CircleShape(Rectangle rect) : base(rect)
+        {
+        }
+
+        public CircleShape(RectangleShape rectangle) : base()
+        {
+        }
+
+
+
+
+        /// <summary>
+        /// Проверка за принадлежност на точка point към правоъгълника.
+        /// В случая на правоъгълник този метод може да не бъде пренаписван, защото
+        /// Реализацията съвпада с тази на абстрактния клас Shape, който проверява
+        /// дали точката е в обхващащия правоъгълник на елемента (а той съвпада с
+        /// елемента в този случай).
+        /// </summary>
+        public override bool Contains(PointF point)
+        {
+            if (base.Contains(point))
+            {
+                float a = Width / 2;
+                float b = Height / 2;
+                float x1 = Location.X + a;
+                float y2 = Location.Y + b;
+
+                return Math.Pow((point.X - x1) / a, 2) + Math.Pow((point.Y - y2) / b, 2) - 1 <= 0;
+
+            }
+
+            else
+                // Ако не е в обхващащия правоъгълник, то неможе да е в обекта и => false
+                return false;
+        }
+
+        /// <summary>
+        /// Частта, визуализираща конкретния примитив.
+        /// </summary>
+        public override void DrawSelf(Graphics grfx)
+        {
+            base.DrawSelf(grfx);
+
+            grfx.FillEllipse(new SolidBrush(FillColor), Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Width);
+            grfx.DrawEllipse(new Pen(BorderColor), Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Width);
+
+        }
+    }
+}
---
+++ src/Model/SquareShape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Draw.src.Model
+{
+    [Serializable]
+    internal class SquareShape : Shape
+    {
+        public SquareShape(RectangleF rect) : base(rect)
+        {
+        }
+
+        public override bool Contains(PointF point)
+        {
+            if (base.Contains(point))
+                return true;
+
+            else
+            {
+                return false;
+            }
+        }
+        public override void DrawSelf(Graphics grfx)
+        {
+            base.DrawSelf(grfx);
+
+            grfx.FillRectangle(new SolidBrush(FillColor), Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Height);
+            grfx.DrawRectangle(new Pen(BorderColor, BorderWidth), Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Height);
+        }
+    }
+}
